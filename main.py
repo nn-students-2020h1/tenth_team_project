3d4423d706ba00c5b9408ecde5a82460cbc13094
6a16ec <85ababobkziQVMew6a16ec>
2020-03-12 04:42:02 +0300

    Full configuration logging all types of errors

--- main.py
+++ main.py
@@ -11,9 +11,6 @@ import settings
 # if __name__ == '__main__':
 #     print(os.getenv("TG_TOKEN"))
 
-# Enable logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
 
 logger = logging.getLogger(__name__)
 
--- settings.py
+++ settings.py
@@ -1,2 +1,62 @@
 from dotenv import load_dotenv
+from logging.config import dictConfig
+
+
 load_dotenv()
+
+LOGGING_CONFIG = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "brief": {
+            "format": "%(message)s",
+        },
+        "usually": {
+            "format": "[%(name)s]: %(message)s",
+        },
+        "default": {
+            "format": "%(asctime)s (%(levelname)s) [%(name)s]: %(message)s",
+            "datefmt": "%Y-%m-%d %H:%M:%S"
+        },
+    },
+     "handlers": {
+        "default": {
+            "formatter": "default",
+            "class": "logging.StreamHandler",
+            "stream": "ext://sys.stdout",
+        },
+        "usually": {
+            "formatter": "usually",
+            "class": "logging.StreamHandler",
+            "stream": "ext://sys.stdout",
+        },
+    },
+    "loggers": {
+        "": {
+            "handlers": ["default"],
+            "propagate": False,
+            "level": "INFO",
+        },
+        "__main__": {
+            "handlers": ["default"],
+            "propagate": False,
+            "level": "DEBUG",
+        },
+        "test": {
+            "handlers": ["default"],
+            "propagate": False,
+            "level": "DEBUG",
+        },
+        "telegram.ext.updater": {
+            "handlers": ["usually"],
+            "propagate": False,
+            "level": "INFO",
+        },
+        "telegram.vendor.ptb_urllib3.urllib3.connectionpool": {
+            "handlers": ["default"],
+            "propagate": False,
+            "level": "ERROR",
+        }
+    }
+}
+dictConfig(LOGGING_CONFIG)
