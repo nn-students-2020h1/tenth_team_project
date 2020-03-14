0ec575de689435e479e9cfe20bd337c3de8a1f52
6a16ec <85ababobkziQVMew6a16ec>
2020-03-14 21:13:13 +0300

    Logging of telegram errors

--- main.py
+++ main.py
@@ -60,11 +60,9 @@ def msg_history(update: Update, context: CallbackContext):
         last_5 = "".join(file.readlines()[-5:])
         update.message.reply_text(last_5)
 
-@msg_logging
 def error(update: Update, context: CallbackContext):
     """Log Errors caused by Updates."""
     logger.warning(f'Update {update} caused error {context.error}')
-
 
 def main():
     bot = Bot(
@@ -80,6 +78,9 @@ def main():
 
     # on noncommand i.e message - echo the message on Telegram
     updater.dispatcher.add_handler(MessageHandler(Filters.text, echo))
+
+    # log all errors
+    updater.dispatcher.add_error_handler(error)
 
     logger.info('start bot')
     # Start the Bot
