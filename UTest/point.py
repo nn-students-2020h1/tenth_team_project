b0d320e62b0e18cddb74f265563eb2866ba48e9d
k-rya <61202227+k-rya@users.noreply.github.com>
2020-04-13 22:02:22 +0300

    Create point.py
    Add class point for tests

---
+++ UTest/point.py
@@ -0,0 +1,27 @@
+import math
+
+
+class Point(object):
+'''Creates a point on a coordinate plane with values x and y.'''
+COUNT = 0
+
+def __init__(self, x, y):
+'''Defines x and y variables'''
+self.X = x
+self.Y = y
+
+def move(self, dx, dy):
+'''Determines where x and y move'''
+self.X = self.X + dx
+self.Y = self.Y + dy
+
+def getX(self):
+return self.X
+
+def getY(self):
+return self.Y
+
+def distance(self, other):
+dx = self.X - other.X
+dy = self.Y - other.Y
+return math.sqrt(dx**2 + dy**2)
